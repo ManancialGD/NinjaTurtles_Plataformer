c36fa7a2ae763ca856d96a9ed7ca4c7477446423
“[Breno <manancialgd@gmail.com>
2024-02-29 11:11:36 +0000

    Changed the camera script for a better one

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,58 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    [SerializeField]
-    private string nomeDoScriptPlayer = "Player";
+    GameObject target;
+    Vector3 velocity = Vector3.zero;
 
-    private Transform alvo;
-    private float newX;
-    private float newY;
-    private Transform CameraTransform;
+    [Range(0, 1)]
+    public float smoothTime;
+    public Vector3 positionOffset;
+    [Header("Axis Limitation")]
+    public Vector2 xLimit;
+    public Vector2 yLimit;
 
-    [SerializeField] public float CameraHeightDisplay;
-    [SerializeField] public float CameraLimitY;
-
-    void Start()
+    private void Awake()
     {
-        // Encontrar o objeto com o script "Player" pelo nome
-        GameObject jogador = GameObject.Find(nomeDoScriptPlayer);
-        CameraTransform = GetComponent<Transform>();
+        // Encontre o objeto pelo nome do script
+        target = GameObject.Find("Player");
 
-        // Verificar se o objeto foi encontrado
-        if (jogador != null)
+        if (target != null)
         {
-            // Obter o componente Transform do objeto "Player"
-            alvo = jogador.GetComponent<Transform>();
-            newX = alvo.position.x;
-            newY = alvo.position.y;
-            transform.position = new Vector3(newX, newY, CameraTransform.position.z);
+            Debug.Log("Player encontrado com sucesso!");
         }
         else
         {
-            Debug.LogError("Objeto com o script Player não encontrado na cena.");
+            Debug.LogError("Erro: Player não encontrado na cena.");
         }
     }
 
-    void Update()
+    private void LateUpdate()
     {
-        // Verificar se o alvo está definido
-        if (alvo != null)
-        {
-            newX += (alvo.position.x - CameraTransform.position.x) / 80;
-
-            if(Mathf.Abs(alvo.position.y + CameraHeightDisplay - CameraTransform.position.y) > CameraLimitY){
-                //Debug.Log(Mathf.Abs(alvo.position.y + CameraHeightDisplay - CameraTransform.position.y));
-                newY += (alvo.position.y + CameraHeightDisplay - CameraTransform.position.y) / 80;
-            } else if(alvo.position.y + CameraHeightDisplay < CameraTransform.position.y){
-                newY += (alvo.position.y + CameraHeightDisplay - CameraTransform.position.y) / 5;
-            }
-            
-
-
-            // Atualizar a posição da câmera para seguir o objeto "Player"
-            transform.position = new Vector3(newX, newY, transform.position.z);
+        Vector3 targetPosition = target.transform.position + positionOffset;
+        targetPosition = new Vector3(Mathf.Clamp(targetPosition.x, xLimit.x, xLimit.y), Mathf.Clamp(targetPosition.y, yLimit.x, yLimit.y), positionOffset.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         }
-    }
 }
