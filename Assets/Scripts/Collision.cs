1548303e4666cc9b5d59ca1bd7cc99bfb624e470
“[Breno <manancialgd@gmail.com>
2024-03-07 11:57:33 +0000

    Remade all player code.

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    [Header("Layers")]
+    public LayerMask jumpableGround;
+
+    [Space]
+
+    public bool onGround;
+    public bool onWall;
+    public bool onRightWall;
+    public bool onLeftWall;
+    public int wallSide;
+
+    [Space]
+
+    [Header("Collision")]
+    public float wallCollisionRadius = 0.25f;
+    public float groundCollisionRadius = 0.25f;
+    public Vector2 bottomOffset, rightOffset, leftOffset;
+    Color debugCollisionColor = Color.red;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, groundCollisionRadius, jumpableGround);
+        onWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, wallCollisionRadius, jumpableGround)
+            || Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, wallCollisionRadius, jumpableGround);
+
+        onRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, wallCollisionRadius, jumpableGround);
+        onLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, wallCollisionRadius, jumpableGround);
+
+        wallSide = onRightWall ? -1 : 1;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = debugCollisionColor;
+
+        Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, groundCollisionRadius); // Ground Collision
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, wallCollisionRadius); // Left wall Collision
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, wallCollisionRadius); // Right wall Collision
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,379 +5,205 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] Sprite CollisionDetectorSprite;
-    [SerializeField] Transform CollisionDetectorGroup;
-    private Rigidbody2D rb;
-    private BoxCollider2D[] coll;
-    private SpriteRenderer sprite;
+    Collision coll;
+    Rigidbody2D rb;
+
+    [Header("Stats")]
+    public float movementSpeed = 10f;
+    public float jumpSpeed = 40f;
+    public float WallJumpForce = 60f;
+    public float slideSpeed = 3f;
+    public float wallSlideLerp = 1.5f;
+
+    [Space]
+
+    [Header("Booleans")]
+    public bool canMove;
+    public bool wallSlide;
+    public bool sliding;
+    public bool wallJumped;
     private Animator anim;
-    private float dirX = 0f;
-    private bool PlayerInAir;
-
-    private int wallSideJumped_history = 0;
-
-    public bool PrintWallState = false;
-
-    // Modificar os parametros de [SerializeField] por código não adianta nada, tem que alterar diretamente do componente
-    [SerializeField] private LayerMask jumpableGround;
-    [SerializeField] private float InitialMoveSpeed;
-    [SerializeField] private float MAX_SPEED;
-    [SerializeField] private float jumpForce;
-    [SerializeField] private float wallBoost;
-    [SerializeField] private float runMultiplier = 1.5f;
-    [SerializeField] private float GravityForce;
+    private SpriteRenderer sprite;
     private CapsuleCollider2D playerCollider;
-
-    public float moveSpeed;
-
+    public bool isWallLocked;
 
     private enum MovementState { Idle, Walk, Jump, Falling, OnWall };
     private string holdOnWallParameter = "HoldOnWall";
-    public bool isWallLocked;
-    public float groundFriction;
-
-    private bool Key_Jump;
-    public float WallSlideSpeed;
-    private float PlayerFinalRotation;
-    public float ROTATION_RATE_DEFAULT;
-
-    // Start is called before the first frame update
     private void Start()
     {
-        PlayerFinalRotation = 0f;
-        Key_Jump = false;
+        coll = GetComponent<Collision>();
+        rb = GetComponent<Rigidbody2D>();
+
         isWallLocked = false;
-        moveSpeed = InitialMoveSpeed;
         playerCollider = GetComponent<CapsuleCollider2D>();
         rb = GetComponent<Rigidbody2D>();
-        coll = GetComponents<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        if (PrintWallState)
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        float xRaw = Input.GetAxisRaw("Horizontal");
+        float yRaw = Input.GetAxisRaw("Vertical");
+        Vector2 dir = new Vector2(x, y);
+
+        Move(dir);
+
+        if(xRaw != 0 && !wallSlide)
         {
-            Debug.Log(wallSideJumped_history);
+            sprite.flipX = xRaw < 0;
         }
 
-        Debug.Log("Payer in air:" + PlayerInAir);
+        if(Input.GetButtonDown("Jump"))
+        {
+            if (coll.onGround)
+            {
+                Jump(Vector2.up);
+            }
+            else if( coll.onWall)
+            {
+                WallJump();
+            }
+        }
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f && !wallJumped && wallSlide)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * .5f);
+        }
 
-        KeyDetection();
+        if( !coll.onGround && coll.onWall)
+        {
+            
+            if( rb.velocity.y > 0)
+            {
+                wallSlide = false;
+                return;
+            }
 
-        GroundDetection();
-        PlayerMovement();
-        WallDetection();
+
+            if( rb.velocity.x > 0 && coll.onRightWall)
+            {
+                if ( xRaw > 0 )
+                {
+                    wallSlide = true;
+                } else
+                {
+                    wallSlide = false;
+                }
+            }
+
+            if (rb.velocity.x < 0 && coll.onLeftWall)
+            {
+                if (xRaw < 0)
+                {
+                    wallSlide = true;
+                }
+                else
+                {
+                    wallSlide = false;
+                }
+            }
+
+            if (wallSlide)
+            {
+                if (!sliding)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, 0);
+                    sliding = true;
+                }
+                rb.velocity = new Vector2(rb.velocity.x, -slideSpeed);
+            }
+
+        }
+
+        if (coll.onGround)
+        {
+            wallSlide = false;
+            sliding = false;
+            wallJumped = false;
+        }
 
         UpdateAnimationState();
-        PlayerAutoRotate();
-
-        //Debug.Log(GetFramesUntilCollision(false));
-
     }
 
-    public void PlayerMovement()
+    private void Move(Vector2 dir)
     {
-        dirX = Input.GetAxisRaw("Horizontal");
-
-        if (isWallLocked) return;
-
-        if (!PlayerInAir)
-        { // IN GROUND
-            moveSpeed = InitialMoveSpeed;
-
-            if (Key_Jump && !PlayerInAir)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            }
-
-            if (!Input.GetButton("Run"))
-            {
-                if (rb.velocity.x + moveSpeed <= MAX_SPEED && dirX == 1)
-                {
-                    rb.velocity = new Vector2(rb.velocity.x + moveSpeed * dirX, rb.velocity.y);
-                }
-                else if (rb.velocity.x + moveSpeed >= -MAX_SPEED && dirX == -1)
-                {
-                    rb.velocity = new Vector2(rb.velocity.x + moveSpeed * dirX, rb.velocity.y);
-                }
-                else
-                {
-                    rb.velocity = new Vector2(MAX_SPEED * dirX, rb.velocity.y);
-                }
-
-            }
-            else
-            { // SHIFT
-
-                if (rb.velocity.x + moveSpeed <= MAX_SPEED * runMultiplier && dirX == 1)
-                {
-                    rb.velocity = new Vector2(rb.velocity.x + moveSpeed * dirX, rb.velocity.y);
-                }
-                else if (rb.velocity.x + moveSpeed >= -MAX_SPEED * runMultiplier && dirX == -1)
-                {
-                    rb.velocity = new Vector2(rb.velocity.x + moveSpeed * dirX, rb.velocity.y);
-                }
-                else
-                {
-                    rb.velocity = new Vector2(MAX_SPEED * runMultiplier * dirX, rb.velocity.y);
-                }
-            }
-        }
-        else
-        { // IN AIR
-            if (rb.velocity.x + moveSpeed > MAX_SPEED && dirX == 1)
-            {
-                rb.velocity = new Vector2(MAX_SPEED * dirX, rb.velocity.y);
-            }
-            else if (rb.velocity.x + moveSpeed < -MAX_SPEED && dirX == -1)
-            {
-                rb.velocity = new Vector2(MAX_SPEED * dirX, rb.velocity.y);
-            }
-            else
-            {
-                rb.velocity = new Vector2(rb.velocity.x + moveSpeed * dirX, rb.velocity.y);
-            }
-        }
-
-    }
-
-    public void KeyDetection()
-    {
-        if (Input.GetButton("Jump") && !Key_Jump) Key_Jump = true;
-        else if (!Input.GetButton("Jump") && Key_Jump) Key_Jump = false;
-    }
-
-    public bool GroundDetection()
-    {
-        if (Physics2D.BoxCast(coll[0].bounds.center, coll[0].bounds.size, 0f, Vector2.down, 0f, jumpableGround))
+        if (!canMove)
         {
-            wallSideJumped_history = 0;
-            if (Input.GetButton("Run"))
-            {
-                if (moveSpeed != InitialMoveSpeed * runMultiplier) moveSpeed = InitialMoveSpeed * runMultiplier;
-            }
-            else
-            {
-                if (moveSpeed != InitialMoveSpeed) moveSpeed = InitialMoveSpeed;
-            }
-            PlayerInAir = false;
-            //GroundFriction();
-            return true;
+            return;
+        }
+
+        if (!wallJumped)
+        {
+            rb.velocity = new Vector2(dir.x * movementSpeed, rb.velocity.y);
         }
         else
         {
-            if (PlayerInAir)
-            {
-                if (moveSpeed * 1.05f <= InitialMoveSpeed)
-                {
-                    moveSpeed = moveSpeed * 1.05f;
-                }
-                else
-                {
-                    moveSpeed = InitialMoveSpeed;
-                }
-            }
-            else
-            { // On Jump
-                PlayerInAir = true;
-                moveSpeed = InitialMoveSpeed / 3;
-            }
-
-            return false;
+            rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(dir.x * movementSpeed, rb.velocity.y)), wallSlideLerp * Time.deltaTime);
         }
+
     }
 
-    public bool GroundFriction()
+    private void Jump(Vector2 dir)
     {
-        if (PlayerInAir) return false;
-        rb.velocity = new Vector2(rb.velocity.x * groundFriction, rb.velocity.y);
-        return true;
+        rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        rb.AddForce(dir * jumpSpeed, ForceMode2D.Impulse);
     }
 
-
-    public int WallDetection()
+    private void WallJump()
     {
-        int wallStatus = GetWallCollision();
+        StartCoroutine(DisableMovement(0.05f));
 
-        if (wallStatus == 0)
-        {
-            isWallLocked = false;
-            if (rb.gravityScale != GravityForce) rb.gravityScale = GravityForce;
-            return 0;
-        }
+        Vector2 wallDir = coll.onRightWall ? Vector2.left : Vector2.right;
+        Jump((Vector2.up + wallDir * WallJumpForce));
 
-        int wallSide = GetWallCollision();
-
-        if (Key_Jump && wallSide != wallSideJumped_history && PlayerInAir)
-        { // Wall Boost
-
-            wallSideJumped_history = wallSide;
-
-            if (wallSide == 1) wallSide = 1;
-            else if (wallSide == 2) wallSide = -1;
-            else Debug.Log("ERRO 986235 - Not touching any wall (Unable to Wall-Jump)");
-            rb.velocity = new Vector2(wallBoost / 2 * wallSide, wallBoost);
-
-            return 2;
-        }
-        else
-        { // In Ground
-
-            if (!Input.GetButton("HoldOnWall") && PlayerInAir)
-            {
-                if (rb.gravityScale != GravityForce) rb.gravityScale = GravityForce;
-                if (rb.velocity.y < -WallSlideSpeed) rb.velocity = new Vector2(rb.velocity.x, -WallSlideSpeed);
-                isWallLocked = false;
-            }
-            else if (PlayerInAir)
-            { //SHIFT
-                isWallLocked = true;
-                if (rb.gravityScale != 0f) rb.gravityScale = 0f;
-                rb.velocity = new Vector2(rb.velocity.x, 0f);
-            }
-            return 1;
-        }
-
+        wallJumped = true;
     }
 
-    private int GetWallCollision()
+    IEnumerator DisableMovement(float time)
     {
-        if (Physics2D.BoxCast(coll[1].bounds.center, coll[1].bounds.size, 0f, Vector2.left, 0.0f, jumpableGround))
-        {
-            return 1;
-        }
-        else if (Physics2D.BoxCast(coll[2].bounds.center, coll[2].bounds.size, 0f, Vector2.right, 0.0f, jumpableGround))
-        {
-            return 2;
-        }
-        else
-        {
-            return 0;
-        }
+        canMove = false;
+        yield return new WaitForSeconds(time);
+        canMove = true;
     }
-
 
     private void UpdateAnimationState()
-    {
-        MovementState state;
-        //Debug.Log(GetFramesUntilCollision(false));
-        
-        if (GetWallCollision() != 0)
         {
-            state = MovementState.OnWall;
-            anim.SetBool(holdOnWallParameter, true);
-            sprite.flipX = GetWallCollision() == 2;
-        }
-        else if (rb.velocity.y > 0.1f || rb.velocity.y < 0.1f && !isNearGround()) 
-        {
-            state = MovementState.Jump;
-            sprite.flipX = rb.velocity.x < 0f;
-            anim.SetBool(holdOnWallParameter, false);
-        }
-        else if (rb.velocity.y < -0.1f && isNearGround())
-        {
-            state = MovementState.Falling;
-            sprite.flipX = rb.velocity.x < 0f;
-            anim.SetBool(holdOnWallParameter, false);
-        }
-        else if (dirX != 0f)
-        {
-            state = MovementState.Walk;
-            sprite.flipX = rb.velocity.x < 0f;
-            anim.SetBool(holdOnWallParameter, false);
-        }
-        else
-        {
-            state = MovementState.Idle;
-            anim.SetBool(holdOnWallParameter, false);
-        }
+            MovementState state;
+            
+            if (coll.onWall)
+            {
+                state = MovementState.OnWall;
+                anim.SetBool(holdOnWallParameter, true);
+                sprite.flipX = coll.onRightWall;
+            }
+            else if (rb.velocity.y > 0.1f || rb.velocity.y < 0.1f && !coll.onGround) 
+            {
+                state = MovementState.Jump;
+                sprite.flipX = rb.velocity.x < 0f;
+                anim.SetBool(holdOnWallParameter, false);
+            }
+            else if (rb.velocity.y < -0.1f && coll.onGround)
+            {
+                state = MovementState.Falling;
+                sprite.flipX = rb.velocity.x < 0f;
+                anim.SetBool(holdOnWallParameter, false);
+            }
+            else if (rb.velocity.x != 0f)
+            {
+                state = MovementState.Walk;
+                sprite.flipX = rb.velocity.x < 0f;
+                anim.SetBool(holdOnWallParameter, false);
+            }
+            else
+            {
+                state = MovementState.Idle;
+                anim.SetBool(holdOnWallParameter, false);
+            }
 
         //Debug.Log(state);
         anim.SetInteger("state", (int)state);
-    }
-
-    private int PlayerAutoRotate()
-    {
-        float player_rotation = rb.rotation;
-        //Debug.Log(player_rotation);
-        if (player_rotation >= 360 || player_rotation <= -360)
-        {
-            rb.rotation = 0f;
-            return 3;
-        }
-        else if (player_rotation == 0f) return 0;
-        else if (player_rotation > 0f)
-        {
-            rb.rotation += ROTATION_RATE_DEFAULT;
-            return 1;
-        }
-        else if (player_rotation < 0f)
-        {
-            rb.rotation -= ROTATION_RATE_DEFAULT;
-            return 2;
-        }
-        else
-        {
-            return 0;
-        }
-
-    }
-
-    private int GetFramesUntilCollision(bool show)
-    {
-
-
-        float velocityX = rb.velocity.x;
-        float velocityY = rb.velocity.y;
-
-        float posX = rb.position.x;
-        float posY = rb.position.y;
-
-        if (show)
-        {
-
-            foreach (Transform child in CollisionDetectorGroup)
-            {
-                Destroy(child.gameObject);
-            }
-
-        }
-
-        for (int i = 0; i < 50; i++)
-        {
-            float newposX = posX + velocityX * (i * 0.14f);
-            float newposY = posY + (velocityY * (i * 0.14f)) - (GravityForce * (i * i) * 0.08f);
-
-            Vector2 newColliderPosition = new Vector2(newposX * 0.1f, newposY * 0.1f);
-
-            //displayX = coll[3].bounds.center.x - PosX;
-            //displayY = coll[3].bounds.center.y - PosY;            
-
-            if (show)
-            {
-                GameObject newSpriteObject = new GameObject("CollisionDetector_" + i);
-                newSpriteObject.transform.SetParent(CollisionDetectorGroup);
-                SpriteRenderer spriteRenderer = newSpriteObject.AddComponent<SpriteRenderer>();
-                spriteRenderer.sprite = CollisionDetectorSprite;
-                newSpriteObject.transform.position = newColliderPosition;
-                newSpriteObject.transform.localScale = new Vector2(0.3f, 0.3f);
-            }
-
-            if (Physics2D.BoxCast(newColliderPosition, coll[3].bounds.size, 0f, Vector2.right, 0.0f, jumpableGround))
-            {
-                return i;
-            }
-        }
-
-        return 0;
-
-    }
-    private bool isNearGround()
-    {
-        return (Physics2D.BoxCast(coll[0].bounds.center, coll[0].bounds.size, 0f, Vector2.down, 3f, jumpableGround));
     }
 
 }
