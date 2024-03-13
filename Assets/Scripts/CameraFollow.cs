9d5718152d32fb0039152479255b63127e016e85
matv <matv.oficial@gmail.com>
2024-03-13 23:31:03 +0000

    Air Moviment fixed + New Camera System

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,39 +4,125 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    Player playerScript; // Reference to the Player script
+    GameObject target;
+
     Vector3 velocity = Vector3.zero;
+
+    //public PlayerChanger p_changer;
 
     [Range(0, 1)]
     public float smoothTime;
     public Vector3 positionOffset;
+
     [Header("Axis Limitation")]
     public Vector2 xLimit;
     public Vector2 yLimit;
 
-    private void Awake()
+    public Vector2 CameraPosition;
+
+    //int currentPlayerTarget;
+
+    Rigidbody2D rb;
+    Transform cameraTranform;
+    Transform playerTransform;
+
+    public float cameraResistance = 0.98f;
+    public float MAX_CAMERA_REACTION;
+    float cameraReaction = 0f; //MAX 1;
+    bool updateCameraReaction = true;
+    float reactionTimer;
+
+    private void Start()
     {
+        cameraReaction = MAX_CAMERA_REACTION;
+        //Debug.Log(p_changer.CurrentPlayerSelected);
         // Encontre o objeto pelo nome do script
-        playerScript = FindObjectOfType<Player>();
+        target = GameObject.Find("Player");
+        playerTransform = target.GetComponent<Transform>();
+        //currentPlayerTarget = 1;
 
         //Logs
-        if (playerScript != null)
+        if (target != null)
         {
             Debug.Log("Player encontrado com sucesso!");
         }
         else
         {
-            Debug.LogError("Erro: Script do Player não encontrado na cena.");
+            Debug.LogError("Erro: Player não encontrado na cena. (target == null)");
+            return;
         }
+        rb = GetComponent<Rigidbody2D>();
+        CameraPosition = new Vector2(playerTransform.position.x, playerTransform.position.y);
+        cameraTranform = GetComponent<Transform>();
     }
 
     private void LateUpdate()
     {
-        if (playerScript != null)
+        if (reactionTimer > 0f) reactionTimer -= Time.deltaTime;
+        if (reactionTimer < 0f)
         {
-            Vector3 targetPosition = playerScript.transform.position + positionOffset;
-            targetPosition = new Vector3(Mathf.Clamp(targetPosition.x, xLimit.x, xLimit.y), Mathf.Clamp(targetPosition.y, yLimit.x, yLimit.y), positionOffset.z);
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+            reactionTimer = 0f;
+            if (!updateCameraReaction) updateCameraReaction = true;
         }
+
+        // Atualizar a "Camera Reaction" para voltar ao normal
+        if (updateCameraReaction && cameraReaction != MAX_CAMERA_REACTION)
+        {
+            if (cameraReaction == 0f) cameraReaction = 0.01f;
+            else if (cameraReaction < MAX_CAMERA_REACTION)
+            {
+                cameraReaction += MAX_CAMERA_REACTION * 0.4f * Time.deltaTime; //99 frames para voltar ao normal.
+                if (cameraReaction > MAX_CAMERA_REACTION) cameraReaction = MAX_CAMERA_REACTION;
+            }
+            else
+            {
+                cameraReaction -= MAX_CAMERA_REACTION * 0.4f * Time.deltaTime; //99 frames para voltar ao normal.
+                if (cameraReaction < MAX_CAMERA_REACTION) cameraReaction = MAX_CAMERA_REACTION;
+            }
+
+        }
+        else if (updateCameraReaction && cameraReaction == MAX_CAMERA_REACTION) Debug.Log("Camera Reaction is MAX");
+
+        //Debug.Log("Player" + p_changer.CurrentPlayerSelected);
+        /*
+        if (p_changer.CurrentPlayerSelected < 1 || p_changer.CurrentPlayerSelected > 2)
+        {
+            Debug.Log("ERRO 854023 - Player index inválido");
+            return;
+        }
+
+        if (currentPlayerTarget != p_changer.CurrentPlayerSelected)
+        {
+            target = GameObject.Find("Player" + p_changer.CurrentPlayerSelected);
+            currentPlayerTarget = p_changer.CurrentPlayerSelected;
+        }
+        */
+
+        playerTransform = target.GetComponent<Transform>();
+        cameraTranform = GetComponent<Transform>();
+        CameraPosition = new Vector2(cameraTranform.position.x, cameraTranform.position.y);
+
+        float distanceX = playerTransform.position.x - CameraPosition.x;
+        float distanceY = playerTransform.position.y - CameraPosition.y;
+
+        float distance = Mathf.Abs(distanceX + distanceY);
+        //Debug.Log("distance = " + distance);
+
+        rb.velocity = new Vector2(rb.velocity.x * cameraResistance + distanceX * cameraReaction, rb.velocity.y * cameraResistance + distanceY * cameraReaction);
+
+    }
+
+    public void BoostCamera(Vector2 force)
+    {
+        rb.velocity = new Vector2(rb.velocity.x + force.x, rb.velocity.y + force.y);
+        return;
+    }
+
+    public void SetCameraReaction(float new_reaction, float update_delay)
+    {
+        cameraReaction = new_reaction;
+        reactionTimer = update_delay;
+        return;
+
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -11,6 +12,7 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     Animator anim;
     SpriteRenderer sprite;
+    [SerializeField] private LayerMask jumpableGround;
 
     static float PLAYER_MAX_AIR_SPEED = 10.0f;
 
@@ -21,7 +23,7 @@ public class Player : MonoBehaviour
     public float movementLerp = 4f;
     public float runMultiplier = 1.5f;
     public float jumpSpeed = 40f;
-    public float WallJumpForce = 60f;
+    public Vector2 WallJumpForce = new Vector2(0.02f, 0.04f);
     public float slideSpeed = 3f;
     public float wallJumpLerp = 4f;
 
@@ -40,6 +42,8 @@ public class Player : MonoBehaviour
     public bool canFlip = true;
     public bool isAttackingLeft = false;
     public bool wasAttackingLeft = false;
+
+    BoxCollider2D[] colliders;
 
     private Collision collisionScript;
 
@@ -98,17 +102,14 @@ public class Player : MonoBehaviour
         {
             if (coll.onGround)
             {
-                Jump(Vector2.up);
+                Jump(jumpSpeed);
             }
             else if (coll.onWall)
             {
                 WallJump();
             }
         }
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f && !wallJumped && wallSlide)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * .5f);
-        }
+
 
         if (!coll.onGround && coll.onWall)
         {
@@ -125,14 +126,14 @@ public class Player : MonoBehaviour
             {
                 if (!sliding)
                 {
-                    rb.velocity = new Vector2(rb.velocity.x, 0);
+                    rb.velocity = new Vector2(rb.velocity.x, 0f);
                     sliding = true;
                 }
 
                 // Lock the y position when holding onto the wall
                 if (isWallLocked && !coll.onGround)
                 {
-                    rb.velocity = new Vector2(rb.velocity.x, 0);
+                    rb.velocity = new Vector2(rb.velocity.x, 0f);
                 }
                 else
                 {
@@ -153,11 +154,14 @@ public class Player : MonoBehaviour
         UpdateAnimationState();
     }
 
-    private void LateUpdate() {
-        if (isAttackingLeft){
+    private void LateUpdate()
+    {
+        if (isAttackingLeft)
+        {
             sprite.flipX = false;
         }
-        else if (!isAttackingLeft && wasAttackingLeft){
+        else if (!isAttackingLeft && wasAttackingLeft)
+        {
             sprite.flipX = true;
         }
 
@@ -179,30 +183,35 @@ public class Player : MonoBehaviour
         if (coll.onGround && !coll.onWall)
         {
             // Player no chão
-            rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(dir.x * currentSpeed, rb.velocity.y)), movementLerp * Time.deltaTime);
+            rb.velocity = new Vector2(rb.velocity.x + dir.x * currentSpeed * Time.deltaTime, rb.velocity.y);
         }
         else if (!coll.onGround)
         {
             // Player no ar
-            if (Mathf.Abs(dir.x) > 0 && rb.velocity.x < PLAYER_MAX_AIR_SPEED) rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(rb.velocity.x + (dir.x * currentSpeed), rb.velocity.y)), wallJumpLerp * Time.deltaTime);
+            if (Mathf.Abs(dir.x) > 0 && Mathf.Abs(rb.velocity.x) < PLAYER_MAX_AIR_SPEED) rb.velocity = new Vector2(rb.velocity.x + dir.x * currentSpeed * Time.deltaTime, rb.velocity.y);
         }
     }
 
 
-    private void Jump(Vector2 dir)
+    private void Jump(float yForce)
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-        rb.AddForce(dir * jumpSpeed, ForceMode2D.Impulse);
+        Debug.Log("Jumped");
+        rb.velocity = new Vector2(rb.velocity.x, yForce);
     }
 
     private void WallJump()
     {
-        StartCoroutine(DisableMovement(0.05f));
 
-        Vector2 wallDir = coll.onRightWall ? Vector2.left : Vector2.right;
-        Jump((Vector2.up + wallDir * WallJumpForce));
+        //StartCoroutine(DisableMovement(0.05f));
+
+        int wallSide = 0;
+        if (coll.onLeftWall) wallSide = 1;
+        else if (coll.onRightWall) wallSide = -1;
 
         wallJumped = true;
+        rb.velocity = new Vector2(WallJumpForce.x * wallSide, WallJumpForce.y);
+        Debug.Log("Wall Jumped" + WallJumpForce.y);
+        return;
     }
 
     IEnumerator DisableMovement(float time)
