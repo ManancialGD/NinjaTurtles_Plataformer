dc6485e1acdf83a0b7825b0de1714fde48546c8a
matv <matv.oficial@gmail.com>
2024-03-13 23:51:09 +0000

    Merge fixed 2

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -9,7 +10,9 @@ public class Player : MonoBehaviour
     // Initiators
     Collision coll;
     Rigidbody2D rb;
+    Animator anim;
     SpriteRenderer sprite;
+    [SerializeField] private LayerMask jumpableGround;
 
     static float PLAYER_MAX_AIR_SPEED = 10.0f;
 
@@ -17,11 +20,11 @@ public class Player : MonoBehaviour
     [Header("Stats")]
     public int damageAmount = 10;
     public float movementSpeed = 10f;
-    public float movementLerp = 4f;
+    public float runMultiplier = 1.5f;
     public float jumpSpeed = 40f;
-    public float WallJumpForce = 60f;
+    public Vector2 WallJumpForce = new Vector2(0.02f, 0.04f);
     public float slideSpeed = 3f;
-    public float wallJumpLerp = 4f;
+
 
     [Space]
 
@@ -30,7 +33,10 @@ public class Player : MonoBehaviour
     public bool wallSlide;
     public bool sliding;
     public bool wallJumped;
+    public bool isRunning;
     public bool isWallLocked;
+    public bool isAttackingLeft = false;
+    public bool wasAttackingLeft = false;
 
     private Collision collisionScript;
 
@@ -39,6 +45,7 @@ public class Player : MonoBehaviour
         coll = GetComponent<Collision>();
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
 
         collisionScript = GetComponent<Collision>();
     }
@@ -55,6 +62,16 @@ public class Player : MonoBehaviour
 
         Move(dir);
 
+        if (Input.GetButtonDown("Run") && coll.onGround)
+        {
+            isRunning = true;
+        }
+        else if (Input.GetButtonUp("Run"))
+        {
+            isRunning = false;
+        }
+        if (coll.onWall && !coll.onGround) isRunning = false;
+
         if (Input.GetButtonDown("HoldOnWall"))
         {
             isWallLocked = true;
@@ -68,17 +85,14 @@ public class Player : MonoBehaviour
         {
             if (coll.onGround)
             {
-                Jump(Vector2.up);
+                Jump(jumpSpeed);
             }
             else if (coll.onWall)
             {
                 WallJump();
             }
         }
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f && !wallJumped && wallSlide)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * .5f);
-        }
+
 
         if (!coll.onGround && coll.onWall)
         {
@@ -95,14 +109,14 @@ public class Player : MonoBehaviour
             {
                 if (!sliding)
                 {
-                    rb.velocity = new Vector2(rb.velocity.x, 0);
+                    rb.velocity = new Vector2(rb.velocity.x, 0f);
                     sliding = true;
                 }
 
                 // Lock the y position when holding onto the wall
                 if (isWallLocked && !coll.onGround)
                 {
-                    rb.velocity = new Vector2(rb.velocity.x, 0);
+                    rb.velocity = new Vector2(rb.velocity.x, 0f);
                 }
                 else
                 {
@@ -119,6 +133,20 @@ public class Player : MonoBehaviour
             wallJumped = false;
         }
         else if (coll.onWall) wallJumped = false;
+
+    }
+
+    private void LateUpdate()
+    {
+        if (isAttackingLeft)
+        {
+            sprite.flipX = false;
+        }
+        else if (!isAttackingLeft && wasAttackingLeft)
+        {
+            sprite.flipX = true;
+        }
+
     }
 
     private void Move(Vector2 dir)
@@ -128,33 +156,44 @@ public class Player : MonoBehaviour
 
         float currentSpeed = movementSpeed;
 
+        if (isRunning)
+        {   // ( isRunning && coll.onGround ) ?
+            // Caso ele o player só consiga acelerar no chão;
+            currentSpeed = movementSpeed * runMultiplier;
+        }
+
         if (coll.onGround && !coll.onWall)
         {
             // Player no chão
-            rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(dir.x * currentSpeed, rb.velocity.y)), movementLerp * Time.deltaTime);
+            rb.velocity = new Vector2(rb.velocity.x + dir.x * currentSpeed * Time.deltaTime, rb.velocity.y);
         }
         else if (!coll.onGround)
         {
             // Player no ar
-            if (Mathf.Abs(dir.x) > 0 && rb.velocity.x < PLAYER_MAX_AIR_SPEED) rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(rb.velocity.x + (dir.x * currentSpeed), rb.velocity.y)), wallJumpLerp * Time.deltaTime);
+            if (Mathf.Abs(dir.x) > 0 && Mathf.Abs(rb.velocity.x) < PLAYER_MAX_AIR_SPEED) rb.velocity = new Vector2(rb.velocity.x + dir.x * currentSpeed * Time.deltaTime, rb.velocity.y);
         }
     }
 
 
-    private void Jump(Vector2 dir)
+    private void Jump(float yForce)
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-        rb.AddForce(dir * jumpSpeed, ForceMode2D.Impulse);
+        Debug.Log("Jumped");
+        rb.velocity = new Vector2(rb.velocity.x, yForce);
     }
 
     private void WallJump()
     {
-        StartCoroutine(DisableMovement(0.05f));
 
-        Vector2 wallDir = coll.onRightWall ? Vector2.left : Vector2.right;
-        Jump((Vector2.up + wallDir * WallJumpForce));
+        //StartCoroutine(DisableMovement(0.05f));
+
+        int wallSide = 0;
+        if (coll.onLeftWall) wallSide = 1;
+        else if (coll.onRightWall) wallSide = -1;
 
         wallJumped = true;
+        rb.velocity = new Vector2(WallJumpForce.x * wallSide, WallJumpForce.y);
+        Debug.Log("Wall Jumped" + WallJumpForce.y);
+        return;
     }
 
     IEnumerator DisableMovement(float time)
@@ -168,4 +207,5 @@ public class Player : MonoBehaviour
     {
         return damageAmount;
     }
+
 }
