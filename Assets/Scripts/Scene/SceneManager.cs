e78fcb4295e6e91711a170c413458506b3fb6417
“[Breno <manancialgd@gmail.com>
2024-06-04 18:29:18 +0100

    Imported TextMeshPro; Made Pause Menu

--- Assets/Scripts/Scene/SceneManager.cs
+++ Assets/Scripts/Scene/SceneManager.cs
@@ -7,13 +7,22 @@ public class SceneManager : MonoBehaviour
     Transform leo;
 
     CameraFollow cameraFollow;
+    PauseMenu pauseMenu;
 
     private void Awake()
     {
         leo = FindObjectOfType<LeoMovement>().transform;
         cameraFollow = FindObjectOfType<CameraFollow>();
-
+        pauseMenu = FindObjectOfType<PauseMenu>(true);
         ChangeCameraTarget(leo);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            pauseMenu.Pause();
+        }
     }
 
     // This is a method for the future, now we set the target as the player in the awake
