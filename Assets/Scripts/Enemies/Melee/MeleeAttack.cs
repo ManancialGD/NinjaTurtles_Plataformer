b20d7446c89e8b98167b65fb1a35c1d720b394a8
“[Breno <manancialgd@gmail.com>
2024-06-09 18:44:03 +0100

    Meelee Attack cooldown and if stunned, can't attack

--- Assets/Scripts/Enemies/Melee/MeleeAttack.cs
+++ Assets/Scripts/Enemies/Melee/MeleeAttack.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 public class MeleeAttack : MonoBehaviour
 {
     MeleeAnimation anim;
+    EnemyHP hp;
     public bool isAttacking;
     [SerializeField] private float attackArea = 80f;
     public bool IsLeoInArea { get; private set; }
-
+    bool canAttack;
 
     private void Awake()
     {
         anim = GetComponent<MeleeAnimation>();
+        hp = GetComponent<EnemyHP>();
+        canAttack = true;
     }
     private void Update()
     {
         // Check if Leo is within the collision radius
         Collider2D hitCollider = Physics2D.OverlapCircle(transform.position, attackArea, LayerMask.GetMask("Leo"));
-        IsLeoInArea = hitCollider != null;
+        if (hitCollider != null) StartCoroutine(WaitALittleToAttack());
+        else IsLeoInArea = false;
 
         if (IsLeoInArea)
         {
-            anim.PlayAnimation("MeleeAttack");
+            if (canAttack && !hp.IsStunned)
+            {
+                anim.PlayAnimation("MeleeAttack");
+                StartCoroutine(AttackCooldown());
+            }
         }
+    }
+
+    IEnumerator AttackCooldown()
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(1);
+        canAttack = true;
+    }
+    IEnumerator WaitALittleToAttack()
+    {
+        yield return new WaitForSeconds(.2f);
+        IsLeoInArea = true;
     }
 }
