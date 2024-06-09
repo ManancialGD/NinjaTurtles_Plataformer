63c3d221b00582e9c9d3af9ca01ff7ae12d4a697
matv <matv.oficial@gmail.com>
2024-06-09 01:22:17 +0100

    Data System (Json)

--- Assets/Scripts/Data/GameData.cs
+++ Assets/Scripts/Data/GameData.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 [System.Serializable]
 public class GameData
 {
-    public int playerKills;
-    public int playerDeaths; 
+    public int Kills { get; set; }
+    public int Deaths { get; set; }
+
+    public GameData()
+    {
+        Kills = 0;
+        Deaths = 0;
+    }
 }
